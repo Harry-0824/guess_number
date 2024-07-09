c9789a3983a11888b9db5b039042b869d94abe03
Harry <prince11290824@gmail.com>
2024-07-09 13:56:17 +0800

    ver2 done

--- guess.py
+++ guess.py
@@ -1,11 +1,13 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+    count += 1
     num = input('請猜數字: ')
     num = int(num)
     if num == r:
-        print('你猜中了!')
+        print('你猜中了!,總共猜了', count, '次')
         break
     elif num > r:
         print('小一點')
@@ -13,4 +15,3 @@ while True:
         print('大一點')
     else:
         print('請輸入1-100的整數')
-
